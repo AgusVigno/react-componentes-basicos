4dc43cc64aeb6a4978fb2a3797609311f900107d
Agustin Vignolo <agus88lp@gmail.com>
2020-06-03 20:21:36 -0300

    Updates

---
+++ precache-manifest.deca1640cbd6c9664154e92d0b7be0ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1635c9a9f96bdc05b464b387861021",
+    "url": "/react-componentes-basicos/index.html"
+  },
+  {
+    "revision": "3a704f8cf1c9dd8d9165",
+    "url": "/react-componentes-basicos/static/css/main.b033c02e.chunk.css"
+  },
+  {
+    "revision": "22312dc31da7446e5828",
+    "url": "/react-componentes-basicos/static/js/2.d4bfb9e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-componentes-basicos/static/js/2.d4bfb9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a704f8cf1c9dd8d9165",
+    "url": "/react-componentes-basicos/static/js/main.389959a9.chunk.js"
+  },
+  {
+    "revision": "4f52ffbd42380c862e46",
+    "url": "/react-componentes-basicos/static/js/runtime-main.611878d3.js"
+  }
+]);
